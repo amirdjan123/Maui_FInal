da8265eabc986791f2c23a1a721fdcf8ffe30b51
amirdjan123 <152814621+amirdjan123@users.noreply.github.com>
2024-04-24 13:04:22 -0600

    Add files via upload

---
+++ MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using MauiApp.Services;
+
+// Assuming "MyMauiApp" is the namespace of your project
+namespace MyMauiApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddSingleton<AuthService>();
+            return builder.Build();
+        }
+    }
+}
